1b5a82e971121ed7aaed867d6737e38d85c03b52
ilabstaff <ilabstaff@172.16.116.71>
2019-01-30 19:27:22 -0800

    REMARKABLE AND TEH RIDDLERH

---
+++ src/Remarkable.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class Remarkable {
+
+	public static void main(String[] args) {
+
+		String power = JOptionPane.showInputDialog("What is your name?");
+				if(power.equals("Marcos")) {
+					JOptionPane.showMessageDialog(null, "Your super power is speed!");
+					}
+					
+	}
--- src/ifs_ints_and_loops/TheRiddler.java
+++ src/ifs_ints_and_loops/TheRiddler.java
@@ -2,12 +2,24 @@ package ifs_ints_and_loops;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
+int score=0;
 
+String color = JOptionPane.showInputDialog("A basket contains 5 apples. Do you know how to divide them among 5 kids so that each one has an apple and one apple stays in the basket?");
+JOptionPane.showInputDialog("What is your answer");
+if(color.equals("4 kids get an apple (one apple for each one of them) and the fifth kid gets an apple with the basket still containing the apple."))
+	JOptionPane.showMessageDialog(null, "CORRECT");
+
+else {
+	JOptionPane.showMessageDialog(null, "WRONG.");
+
+		
+		
+		
 		// 1. Make a variable to hold the score
 
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
